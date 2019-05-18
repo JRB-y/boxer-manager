90971efce440b2d81807d4e69e12f2c7cde54aa1
jrb <jrb.youssef@gmail.com>
2019-05-19 00:16:26 +0100

    refactor TatamiController

--- app/Http/Controllers/Tatami/TatamiController.php
+++ app/Http/Controllers/Tatami/TatamiController.php
@@ -6,39 +6,21 @@ use App\Boxer;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\Boxers\BoxersRepository;
 
 class TatamiController extends Controller
 {
+
+    protected $tatami;
+    protected $category = "tatami";
+
+    public function __construct()
+    {
+        $this->boxer = new BoxersRepository(new Boxer());
+    }
     public function index()
     {
-        $boxersTatami = Boxer::whereHas('types', function($q){
-            $q->where('category', 'tatami');
-        })->with('types')->get();
-
-        $poussin = $boxersTatami->filter(function($val , $key){
-            return $val['birth_date']->age >= 8 && $val['birth_date']->age <= 9;
-        });
-
-        $ecole = $boxersTatami->filter(function($val , $key){
-            return $val['birth_date']->age >= 10 && $val['birth_date']->age <= 12;
-        });
-
-        $minime = $boxersTatami->filter(function($val , $key){
-            return $val['birth_date']->age >= 13 && $val['birth_date']->age <= 14;
-        });
-
-        $cadet = $boxersTatami->filter(function($val , $key){
-            return $val['birth_date']->age >= 15 && $val['birth_date']->age <= 16;
-        });
-
-        $junior = $boxersTatami->filter(function($val , $key){
-            return $val['birth_date']->age >= 17 && $val['birth_date']->age <= 18;
-        });
-
-        $senior = $boxersTatami->filter(function($val , $key){
-            return $val['birth_date']->age >= 19;
-        });
-
-        return view('tatami.index', compact('poussin', 'ecole', 'minime', 'cadet', 'junior', 'senior'));
+        $boxersTatami = $this->boxer->ofCategory($this->category);
+        return view('tatami.index', $this->boxer->filterAges($boxersTatami));
     }
 }
